26f22f6717dbfd7d0d7732c3cf938feb9249c894
Roland Estropia Jr <restropiajr@gmail.com>
2023-09-12 20:18:27 -0700

    Created custom css selector for main containers

--- game-explorer/app/page.tsx
+++ game-explorer/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main>
-			<section className='flex flex-col justify-center items-center min-h-screen w-full bg-[#151515]'>
+			<section className='main-container'>
 				<h1 className='text-3xl text-white'>Home</h1>
 			</section>
 		</main>

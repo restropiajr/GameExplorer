1ef9ff9adfc90d8b9e3d12b44fac6088e4777280
Roland Estropia Jr <restropiajr@gmail.com>
2023-09-18 03:31:07 -0700

    Created better home entrance cover

--- game-explorer/app/page.tsx
+++ game-explorer/app/page.tsx
@@ -1,25 +1,38 @@
+'use client';
 import { BsSearch } from 'react-icons/bs';
+import { Link as ScrollLink } from 'react-scroll';
 
 export default function Home() {
   return (
-    <main className='main-container'>
-      <section className='mt-24 flex flex-col items-center justify-center gap-4'>
-        <h1 className='text-center text-4xl text-white md:text-5xl'>
+    <main>
+      <section className='main-container gap-4'>
+        <h1 className='text-center text-5xl text-white md:text-5xl'>
           Welcome to {''}
           <span className='animate-gradient gradient-text font-bold tracking-wide'>
             GameExplorer
           </span>
         </h1>
         <p className='text-white'>Over 500,000+ video games to discover!</p>
+        <ScrollLink
+          to='games'
+          smooth={true}
+          duration={500}
+          ignoreCancelEvents={true}
+          className='cursor-pointer'
+        >
+          <p className='rounded border px-4 py-2 text-xl text-white'>
+            See Collection
+          </p>
+        </ScrollLink>
+      </section>
+      <section id='games' className='main-container gap-4'>
+        <h2 className='text-2xl text-white underline'>Latest Releases</h2>
         <form className='flex gap-4'>
           <input type='search' className='rounded p-2' placeholder='Search' />
           <button type='button'>
             <BsSearch className='text-white' size={25} />
           </button>
         </form>
-      </section>
-      <section className='mt-24 flex w-full flex-col items-center justify-center gap-4 md:w-2/3 md:items-start'>
-        <h2 className='text-2xl text-white underline'>Latest Releases</h2>
       </section>
     </main>
   );
